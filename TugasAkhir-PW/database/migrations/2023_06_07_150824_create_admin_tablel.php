57b21e632710eb9f7f9adea48e94e0495357986c
rizviroshan10 <rizviroshan10@mhs.mdp.ac.id>
2023-06-07 23:26:06 +0700

    ada typo pas buat migration

--- TugasAkhir-PW/database/migrations/2023_06_07_150824_create_admin_tablel.php
+++ TugasAkhir-PW/database/migrations/2023_06_07_150824_create_admin_tablel.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('admin_tablel', function (Blueprint $table) {
+        Schema::create('admin', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('admin_tablel');
+        Schema::dropIfExists('admin');
     }
 };
