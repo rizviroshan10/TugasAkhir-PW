fbbc5cceaad4cd796bf15577f458edde7b6201b6
rizviroshan10 <rizviroshan10@mhs.mdp.ac.id>
2023-06-15 14:47:24 +0700

    dashboard

--- TugasAkhir-PW/resources/views/layout/main.blade.php
+++ TugasAkhir-PW/resources/views/layout/main.blade.php
@@ -84,7 +84,7 @@
             <span class="nav-link">Navigation</span>
           </li>
           <li class="nav-item menu-items">
-            <a class="nav-link" href="{{asset('index.html')}}">
+            <a class="nav-link" href="{{route('dashboard')}}">
               <span class="menu-icon">
                 <i class="mdi mdi-speedometer"></i>
               </span>
